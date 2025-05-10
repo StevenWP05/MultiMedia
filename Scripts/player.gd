9ce3b97ba818c9c6467e4940a59ace98d67da421
StevenWP5 <stevenvictory05@gmail.com>
2025-05-10 15:33:14 +0700

    Sprint and flashlight

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,25 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+var SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+var sprint_slider
 
+func _ready():
+	sprint_slider = get_node("/root/" + get_tree().current_scene.name + "/UI/sprint_slider")
 
-func _physics_process(delta: float) -> void:
+func _process(delta):
+	if SPEED == 8:
+		sprint_slider.value = sprint_slider.value - 0.3 * delta
+		if sprint_slider.value < sprint_slider.min_value:
+			SPEED -= 3
+	if SPEED != 8:
+		if sprint_slider.value < sprint_slider.max_value:
+			sprint_slider.value = sprint_slider.value + 0.4 * delta
+		if sprint_slider.value == sprint_slider.max_value:
+			sprint_slider.visible = false
+
+func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -21,6 +35,13 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		
+		if Input.is_action_just_pressed("sprint"):
+			sprint_slider.visible = true
+			SPEED += 3
+		if Input.is_action_just_released("sprint"):
+			SPEED -=3
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
