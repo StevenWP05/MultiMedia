3065e2af965c8ba3669d6082aa17a5d41d66ce16
StevenWP5 <stevenvictory05@gmail.com>
2025-06-12 17:09:01 +0700

    fre

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,16 +1,21 @@
 extends Control
 
 @onready var click_sound = $ClickSound
-
+@onready var settings_panel = preload("res://Settings.tscn").instantiate()
+func _ready():
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	add_child(settings_panel)
+	settings_panel.process_mode = Node.PROCESS_MODE_ALWAYS
+	settings_panel.hide()
 func _on_start_pressed() -> void:
 	click_sound.play()
 	await get_tree().create_timer(0.2).timeout  # short delay so sound isn't cut off
-	get_tree().change_scene_to_file("res://node_3d.tscn")
+	get_tree().change_scene_to_file("res://cotrols.tscn")
 
 func _on_settings_pressed() -> void:
 	click_sound.play()
 	await get_tree().create_timer(0.2).timeout
-	get_tree().change_scene_to_file("res://settings.tscn")
+	settings_panel.show()
 	# Add your settings menu logic here
 
 func _on_exit_pressed() -> void:
--- pause.gd
+++ pause.gd
@@ -1,13 +1,29 @@
 extends Control
 
 @onready var click_sound = $ClickSound
+@onready var settings_panel = preload("res://Settings.tscn").instantiate()
+
+func _ready():
+	add_child(settings_panel)
+	settings_panel.process_mode = Node.PROCESS_MODE_ALWAYS
+	settings_panel.hide()
+
 func _on_resume_pressed() -> void:
-	#print("Resume pressed") 
 	click_sound.play()
-	await get_tree().create_timer(0.2).timeout 
+	await get_tree().create_timer(0.2).timeout
 	get_tree().paused = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	visible = false # Replace with function body.
+	visible = false
+
+	var camera = get_tree().get_root().find_child("Camera3D", true, false)
+	if camera:
+		print("Camera found:", camera)
+		if camera.has_method("force_resume_camera"):
+			camera.force_resume_camera()
+		else:
+			print("Method not found on camera!")
+	else:
+		print("Camera node not found!")
 
 
 func _on_leave_pressed() -> void:
@@ -16,3 +32,9 @@ func _on_leave_pressed() -> void:
 	get_tree().paused = false  # Unpause the game
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	get_tree().change_scene_to_file("res://main_menu.tscn") # Replace with function body.
+
+
+func _on_settings_pressed() -> void:
+	click_sound.play()
+	await get_tree().create_timer(0.2).timeout
+	settings_panel.show()
